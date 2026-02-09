2e1623270bc361749eedf8166e3823c64e8ac10d
Emma Ronai <emma.c.ronail@example.com>
2026-02-09 16:02:33 -0600

    pin change test

--- ros2_ws/pin_test_script.py
+++ ros2_ws/pin_test_script.py
@@ -1,7 +1,7 @@
 from gpiozero import Motor
 from time import sleep
 
-motor = Motor(forward="GPIO23", backward="GPIO24")
+motor = Motor(forward="GPIO25", backward="GPIO26")
 motor.forward()
 sleep(2)
 motor.backward()
